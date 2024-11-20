abc583d5c816350343acb96bef36938961de6fed
Danil_frolov <danil.frolov20201@gmail.com>
2024-11-20 05:28:28 +0300

    lesson4

---
+++ lesson4/Data/BookContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using lesson4.Models;
+
+namespace lesson4.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<lesson4.Models.Book> Book { get; set; } = default!;
+    }
+}
